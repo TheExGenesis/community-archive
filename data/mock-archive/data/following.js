4b647c3eae1325270a9086801d669966d515192f
TheExGenesis <theexgenesis@gmail.com>
2024-07-31 13:05:03 +0100

    test: mock archive

---
+++ data/mock-archive/data/following.js
@@ -0,0 +1,20 @@
+window.YTD.following.part0 = [
+  {
+    following: {
+      accountId: '824308056351735809',
+      userLink: 'https://twitter.com/intent/user?user_id=824308056351735809',
+    },
+  },
+  {
+    following: {
+      accountId: '18969923',
+      userLink: 'https://twitter.com/intent/user?user_id=18969923',
+    },
+  },
+  {
+    following: {
+      accountId: '3297675443',
+      userLink: 'https://twitter.com/intent/user?user_id=3297675443',
+    },
+  },
+]
---
+++ data/mock-archive/data/tweets.js
@@ -0,0 +1,477 @@
+window.YTD.tweets.part0 = [
+  {
+    tweet: {
+      edit_info: {
+        initial: {
+          editTweetIds: ['1627031510963441664'],
+          editableUntil: '2023-02-18T20:15:15.000Z',
+          editsRemaining: '5',
+          isEditEligible: false,
+        },
+      },
+      retweeted: false,
+      source:
+        '<a href="https://mobile.twitter.com" rel="nofollow">Twitter Web App</a>',
+      entities: {
+        hashtags: [],
+        symbols: [],
+        user_mentions: [],
+        urls: [],
+      },
+      display_text_range: ['0', '200'],
+      favorite_count: '1',
+      in_reply_to_status_id_str: '1627019761950375936',
+      id_str: '1627031510963441664',
+      in_reply_to_user_id: '322603863',
+      truncated: false,
+      retweet_count: '0',
+      id: '1627031510963441664',
+      in_reply_to_status_id: '1627019761950375936',
+      created_at: 'Sat Feb 18 19:45:15 +0000 2023',
+      favorited: false,
+      full_text:
+        "Another lesson is: there's a lot of trash, many not-very-contentful tweets, and it's not trivial to separate them, but we can always train a classifier, or honestly just use an LLM w few-shot examples",
+      lang: 'en',
+      in_reply_to_screen_name: 'exGenesis',
+      in_reply_to_user_id_str: '322603863',
+    },
+  },
+  {
+    tweet: {
+      edit_info: {
+        initial: {
+          editTweetIds: ['1627019761950375936'],
+          editableUntil: '2023-02-18T19:28:34.000Z',
+          editsRemaining: '5',
+          isEditEligible: false,
+        },
+      },
+      retweeted: false,
+      source:
+        '<a href="https://mobile.twitter.com" rel="nofollow">Twitter Web App</a>',
+      entities: {
+        user_mentions: [],
+        urls: [],
+        symbols: [],
+        media: [
+          {
+            expanded_url:
+              'https://twitter.com/exGenesis/status/1627019761950375936/photo/1',
+            indices: ['280', '303'],
+            url: 'https://t.co/XOWm3INJng',
+            media_url: 'http://pbs.twimg.com/media/FpRUnGtX0AEvwY1.jpg',
+            id_str: '1627018396180140033',
+            id: '1627018396180140033',
+            media_url_https: 'https://pbs.twimg.com/media/FpRUnGtX0AEvwY1.jpg',
+            sizes: {
+              medium: {
+                w: '1176',
+                h: '898',
+                resize: 'fit',
+              },
+              small: {
+                w: '680',
+                h: '519',
+                resize: 'fit',
+              },
+              thumb: {
+                w: '150',
+                h: '150',
+                resize: 'crop',
+              },
+              large: {
+                w: '1176',
+                h: '898',
+                resize: 'fit',
+              },
+            },
+            type: 'photo',
+            display_url: 'pic.twitter.com/XOWm3INJng',
+          },
+        ],
+        hashtags: [],
+      },
+      display_text_range: ['0', '303'],
+      favorite_count: '3',
+      in_reply_to_status_id_str: '1626699961042698240',
+      id_str: '1627019761950375936',
+      in_reply_to_user_id: '322603863',
+      truncated: false,
+      retweet_count: '0',
+      id: '1627019761950375936',
+      in_reply_to_status_id: '1626699961042698240',
+      possibly_sensitive: false,
+      created_at: 'Sat Feb 18 18:58:34 +0000 2023',
+      favorited: false,
+      full_text:
+        "into the thick of it!\n\nsome lessons:\n\nWe don't want clustering we want TOPIC MODELLING, tweets can be about more than 1 thing\n\nalso, topic modelling my tweets is HARD, my best stuff is interdisciplinary and that sends models for a loop\n\nanyway we got some coherent topics!!!!!!!! https://t.co/XOWm3INJng",
+      lang: 'en',
+      in_reply_to_screen_name: 'exGenesis',
+      in_reply_to_user_id_str: '322603863',
+      extended_entities: {
+        media: [
+          {
+            expanded_url:
+              'https://twitter.com/exGenesis/status/1627019761950375936/photo/1',
+            indices: ['280', '303'],
+            url: 'https://t.co/XOWm3INJng',
+            media_url: 'http://pbs.twimg.com/media/FpRUnGtX0AEvwY1.jpg',
+            id_str: '1627018396180140033',
+            id: '1627018396180140033',
+            media_url_https: 'https://pbs.twimg.com/media/FpRUnGtX0AEvwY1.jpg',
+            sizes: {
+              medium: {
+                w: '1176',
+                h: '898',
+                resize: 'fit',
+              },
+              small: {
+                w: '680',
+                h: '519',
+                resize: 'fit',
+              },
+              thumb: {
+                w: '150',
+                h: '150',
+                resize: 'crop',
+              },
+              large: {
+                w: '1176',
+                h: '898',
+                resize: 'fit',
+              },
+            },
+            type: 'photo',
+            display_url: 'pic.twitter.com/XOWm3INJng',
+          },
+          {
+            expanded_url:
+              'https://twitter.com/exGenesis/status/1627019761950375936/photo/1',
+            indices: ['280', '303'],
+            url: 'https://t.co/XOWm3INJng',
+            media_url: 'http://pbs.twimg.com/media/FpRUuz6X0AETb-X.jpg',
+            id_str: '1627018528573345793',
+            id: '1627018528573345793',
+            media_url_https: 'https://pbs.twimg.com/media/FpRUuz6X0AETb-X.jpg',
+            sizes: {
+              medium: {
+                w: '1176',
+                h: '898',
+                resize: 'fit',
+              },
+              small: {
+                w: '680',
+                h: '519',
+                resize: 'fit',
+              },
+              thumb: {
+                w: '150',
+                h: '150',
+                resize: 'crop',
+              },
+              large: {
+                w: '1176',
+                h: '898',
+                resize: 'fit',
+              },
+            },
+            type: 'photo',
+            display_url: 'pic.twitter.com/XOWm3INJng',
+          },
+          {
+            expanded_url:
+              'https://twitter.com/exGenesis/status/1627019761950375936/photo/1',
+            indices: ['280', '303'],
+            url: 'https://t.co/XOWm3INJng',
+            media_url: 'http://pbs.twimg.com/media/FpRVfznXoAICT86.jpg',
+            id_str: '1627019370307231746',
+            id: '1627019370307231746',
+            media_url_https: 'https://pbs.twimg.com/media/FpRVfznXoAICT86.jpg',
+            sizes: {
+              medium: {
+                w: '1200',
+                h: '760',
+                resize: 'fit',
+              },
+              small: {
+                w: '680',
+                h: '431',
+                resize: 'fit',
+              },
+              thumb: {
+                w: '150',
+                h: '150',
+                resize: 'crop',
+              },
+              large: {
+                w: '2048',
+                h: '1298',
+                resize: 'fit',
+              },
+            },
+            type: 'photo',
+            display_url: 'pic.twitter.com/XOWm3INJng',
+          },
+          {
+            expanded_url:
+              'https://twitter.com/exGenesis/status/1627019761950375936/photo/1',
+            indices: ['280', '303'],
+            url: 'https://t.co/XOWm3INJng',
+            media_url: 'http://pbs.twimg.com/media/FpRV09rWIAEBXjz.jpg',
+            id_str: '1627019733785518081',
+            id: '1627019733785518081',
+            media_url_https: 'https://pbs.twimg.com/media/FpRV09rWIAEBXjz.jpg',
+            sizes: {
+              medium: {
+                w: '1200',
+                h: '760',
+                resize: 'fit',
+              },
+              small: {
+                w: '680',
+                h: '431',
+                resize: 'fit',
+              },
+              thumb: {
+                w: '150',
+                h: '150',
+                resize: 'crop',
+              },
+              large: {
+                w: '2048',
+                h: '1298',
+                resize: 'fit',
+              },
+            },
+            type: 'photo',
+            display_url: 'pic.twitter.com/XOWm3INJng',
+          },
+        ],
+      },
+    },
+  },
+  {
+    tweet: {
+      edit_info: {
+        initial: {
+          editTweetIds: ['1626922779105759235'],
+          editableUntil: '2023-02-18T13:03:11.000Z',
+          editsRemaining: '5',
+          isEditEligible: false,
+        },
+      },
+      retweeted: false,
+      source:
+        '<a href="https://mobile.twitter.com" rel="nofollow">Twitter Web App</a>',
+      entities: {
+        hashtags: [],
+        symbols: [],
+        user_mentions: [
+          {
+            name: 'Cameron (🇵🇹)',
+            screen_name: 'empathy2000',
+            indices: ['0', '12'],
+            id_str: '732980797985148928',
+            id: '732980797985148928',
+          },
+        ],
+        urls: [],
+      },
+      display_text_range: ['0', '71'],
+      favorite_count: '1',
+      in_reply_to_status_id_str: '1626916612560281601',
+      id_str: '1626922779105759235',
+      in_reply_to_user_id: '732980797985148928',
+      truncated: false,
+      retweet_count: '0',
+      id: '1626922779105759235',
+      in_reply_to_status_id: '1626916612560281601',
+      created_at: 'Sat Feb 18 12:33:11 +0000 2023',
+      favorited: false,
+      full_text:
+        "@empathy2000 did NOT know but now I'm listening to her solo act bc of u",
+      lang: 'en',
+      in_reply_to_screen_name: 'empathy2000',
+      in_reply_to_user_id_str: '732980797985148928',
+    },
+  },
+  {
+    tweet: {
+      edit_info: {
+        initial: {
+          editTweetIds: ['1626908498134020098'],
+          editableUntil: '2023-02-18T12:06:27.000Z',
+          editsRemaining: '5',
+          isEditEligible: false,
+        },
+      },
+      retweeted: false,
+      source:
+        '<a href="https://mobile.twitter.com" rel="nofollow">Twitter Web App</a>',
+      entities: {
+        hashtags: [],
+        symbols: [],
+        user_mentions: [
+          {
+            name: 'Cameron (🇵🇹)',
+            screen_name: 'empathy2000',
+            indices: ['0', '12'],
+            id_str: '732980797985148928',
+            id: '732980797985148928',
+          },
+        ],
+        urls: [],
+      },
+      display_text_range: ['0', '25'],
+      favorite_count: '1',
+      in_reply_to_status_id_str: '1626904491957133312',
+      id_str: '1626908498134020098',
+      in_reply_to_user_id: '732980797985148928',
+      truncated: false,
+      retweet_count: '0',
+      id: '1626908498134020098',
+      in_reply_to_status_id: '1626904491957133312',
+      created_at: 'Sat Feb 18 11:36:27 +0000 2023',
+      favorited: false,
+      full_text: "@empathy2000 I'm obsessed",
+      lang: 'en',
+      in_reply_to_screen_name: 'empathy2000',
+      in_reply_to_user_id_str: '732980797985148928',
+    },
+  },
+  {
+    tweet: {
+      edit_info: {
+        initial: {
+          editTweetIds: ['1626902980006932483'],
+          editableUntil: '2023-02-18T11:44:31.000Z',
+          editsRemaining: '5',
+          isEditEligible: true,
+        },
+      },
+      retweeted: false,
+      source:
+        '<a href="http://twitter.com/download/android" rel="nofollow">Twitter for Android</a>',
+      entities: {
+        hashtags: [],
+        symbols: [],
+        user_mentions: [],
+        urls: [],
+      },
+      display_text_range: ['0', '122'],
+      favorite_count: '8',
+      id_str: '1626902980006932483',
+      truncated: false,
+      retweet_count: '0',
+      id: '1626902980006932483',
+      created_at: 'Sat Feb 18 11:14:31 +0000 2023',
+      favorited: false,
+      full_text:
+        'music update:\n\nmy Spotify history for the past 48h has consisted solely of Caroline polacheck, jockstrap, and the pom-poms',
+      lang: 'en',
+    },
+  },
+  {
+    tweet: {
+      edit_info: {
+        initial: {
+          editTweetIds: ['1626871062343327744'],
+          editableUntil: '2023-02-18T09:37:41.000Z',
+          editsRemaining: '5',
+          isEditEligible: false,
+        },
+      },
+      retweeted: false,
+      source:
+        '<a href="http://twitter.com/download/android" rel="nofollow">Twitter for Android</a>',
+      entities: {
+        hashtags: [],
+        symbols: [],
+        user_mentions: [
+          {
+            name: '🐇🌷bosco🌷🐇',
+            screen_name: 'selentelechia',
+            indices: ['0', '14'],
+            id_str: '990430425825755138',
+            id: '990430425825755138',
+          },
+          {
+            name: 'Mark',
+            screen_name: 'meditationstuff',
+            indices: ['15', '31'],
+            id_str: '2587393812',
+            id: '2587393812',
+          },
+        ],
+        urls: [],
+      },
+      display_text_range: ['0', '54'],
+      favorite_count: '1',
+      in_reply_to_status_id_str: '1626870349575880705',
+      id_str: '1626871062343327744',
+      in_reply_to_user_id: '990430425825755138',
+      truncated: false,
+      retweet_count: '0',
+      id: '1626871062343327744',
+      in_reply_to_status_id: '1626870349575880705',
+      created_at: 'Sat Feb 18 09:07:41 +0000 2023',
+      favorited: false,
+      full_text: '@selentelechia @meditationstuff omg me for like a year',
+      lang: 'en',
+      in_reply_to_screen_name: 'selentelechia',
+      in_reply_to_user_id_str: '990430425825755138',
+    },
+  },
+  {
+    tweet: {
+      edit_info: {
+        initial: {
+          editTweetIds: ['1626745546156257281'],
+          editableUntil: '2023-02-18T01:18:56.000Z',
+          editsRemaining: '5',
+          isEditEligible: false,
+        },
+      },
+      retweeted: false,
+      source:
+        '<a href="http://twitter.com/download/android" rel="nofollow">Twitter for Android</a>',
+      entities: {
+        hashtags: [],
+        symbols: [],
+        user_mentions: [
+          {
+            name: 'curious irrationalist {0/100 longform-ish things}',
+            screen_name: '42irrationalist',
+            indices: ['0', '16'],
+            id_str: '1248684884790587393',
+            id: '1248684884790587393',
+          },
+          {
+            name: 'roon',
+            screen_name: 'tszzl',
+            indices: ['17', '23'],
+            id_str: '1460283925',
+            id: '1460283925',
+          },
+        ],
+        urls: [],
+      },
+      display_text_range: ['0', '70'],
+      favorite_count: '12',
+      in_reply_to_status_id_str: '1626745216307720192',
+      id_str: '1626745546156257281',
+      in_reply_to_user_id: '1248684884790587393',
+      truncated: false,
+      retweet_count: '0',
+      id: '1626745546156257281',
+      in_reply_to_status_id: '1626745216307720192',
+      created_at: 'Sat Feb 18 00:48:56 +0000 2023',
+      favorited: false,
+      full_text:
+        "@42irrationalist @tszzl They're called Russian mountains in Portuguese",
+      lang: 'en',
+      in_reply_to_screen_name: '42irrationalist',
+      in_reply_to_user_id_str: '1248684884790587393',
+    },
+  },
+]

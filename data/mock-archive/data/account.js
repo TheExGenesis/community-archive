b52871aa1ce1edd941e97ec6134ed600cde9f069
TheExGenesis <theexgenesis@gmail.com>
2024-07-31 13:05:03 +0100

    test: mock archive

---
+++ data/mock-archive/data/account.js
@@ -0,0 +1,12 @@
+window.YTD.account.part0 = [
+  {
+    account: {
+      email: 'theexgenesis@gmail.com',
+      createdVia: 'web',
+      username: 'exGenesis',
+      accountId: '322603863',
+      createdAt: '2011-06-23T13:04:14.000Z',
+      accountDisplayName: '❤️‍🔥 xiq',
+    },
+  },
+]

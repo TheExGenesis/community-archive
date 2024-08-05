f86397e1dafaa76447cf534eaef3b24ce9bbabaf
TheExGenesis <theexgenesis@gmail.com>
2024-08-05 13:01:14 +0100

    ci: don't lint mock data

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,7 @@
 const path = require('path')
-
 const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames
+    .filter((f) => !f.includes('mock-archive'))
     .map((f) => path.relative(process.cwd(), f))
     .join(' --file ')}`
 

d2bc3c4934fa4fa162984c8bc68c9874a05d13ed
TheExGenesis <theexgenesis@gmail.com>
2024-08-14 23:06:45 +0100

    feat: upload archive from client side

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,30 +2,23 @@ import AuthButton from '@/components/AuthButton'
 import ConnectSupabaseSteps from '@/components/ConnectSupabaseSteps'
 import SignUpUserSteps from '@/components/SignUpUserSteps'
 import Header from '@/components/Header'
+import ThemeToggle from '@/components/ThemeToggle'
 import { cookies } from 'next/headers'
 import { createServerClient } from '@/utils/supabase'
-import ThemeToggle from '@/components/ThemeToggle'
 import UploadTwitterArchive from '@/components/UploadTwitterArchive'
 
+import { redirect } from 'next/navigation'
+
 export default async function Index() {
-  const cookieStore = cookies()
-
-  const canInitSupabaseClient = () => {
-    // This function is just for the interactive tutorial.
-    // Feel free to remove it once you have Supabase connected.
-    try {
-      createServerClient(cookieStore)
-      return true
-    } catch (e) {
-      return false
-    }
-  }
-
-  const isSupabaseConnected = canInitSupabaseClient()
-
-  return (
-    <div className="flex w-full flex-1 flex-col items-center gap-20">
-      <UploadTwitterArchive />
-    </div>
-  )
+  redirect('/upload-archive')
 }
+
+// export default async function Index() {
+//   const cookieStore = cookies()
+
+//   return (
+//     <div className="flex w-full flex-1 flex-col items-center gap-20">
+//       <UploadTwitterArchive />
+//     </div>
+//   )
+// }
---
+++ src/app/upload-archive/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+import UploadTwitterArchive from '@/components/UploadTwitterArchive'
+
+export default function UploadArchivePage() {
+  return (
+    <div className="flex w-full flex-1 flex-col items-center gap-20">
+      <UploadTwitterArchive />
+    </div>
+  )
+}

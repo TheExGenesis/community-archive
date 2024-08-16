651427eaf59b8a819c96b0bbbe719a905fc69ab3
TheExGenesis <theexgenesis@gmail.com>
2024-08-16 18:12:03 +0100

    mock data formatting

--- data/mock-archive/data/profile.js
+++ data/mock-archive/data/profile.js
@@ -1,15 +1,15 @@
 window.YTD.profile.part0 = [
   {
-    profile: {
-      description: {
-        bio: 'negentropy',
-        website: 'https://t.co/n44CGMQVGL',
-        location: 'Porto, Portugal ',
+    "profile": {
+      "description": {
+        "bio": "negentropy",
+        "website": "https://t.co/n44CGMQVGL",
+        "location": "Porto, Portugal "
       },
-      avatarMediaUrl:
-        'https://pbs.twimg.com/profile_images/1562836797494906880/K_O23TKw.jpg',
-      headerMediaUrl:
-        'https://pbs.twimg.com/profile_banners/322603863/1671291348',
-    },
-  },
+      "avatarMediaUrl":
+        "https://pbs.twimg.com/profile_images/1562836797494906880/K_O23TKw.jpg",
+      "headerMediaUrl":
+        "https://pbs.twimg.com/profile_banners/322603863/1671291348"
+    }
+  }
 ]

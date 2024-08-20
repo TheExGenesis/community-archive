39826a6e8928b18560f7c9dcc3f9d0ec18ab733a
TheExGenesis <theexgenesis@gmail.com>
2024-08-20 18:37:59 +0100

    feat: neater interface with stats and rollback and visible error if archive fails to upload

--- src/components/CommunityStats.tsx
+++ src/components/CommunityStats.tsx
@@ -2,40 +2,56 @@ import React, { useEffect, useState } from 'react'
 import { createBrowserClient } from '@/utils/supabase'
 import { getTableName } from '@/lib-client/getTableName'
 
-const getUsernames = async (supabase: any) => {
-  const { data: accounts, error } = await supabase
+const getStats = async (supabase: any) => {
+  const { data: accounts, error: accountsError } = await supabase
     .from(getTableName('account'))
-    .select(`*`)
+    .select(`username`)
     .order('created_at', { ascending: false })
-  console.log({ accounts })
-  if (error) {
-    console.error('Error fetching tweets:', error)
-    throw error
+
+  const { count: tweetCount, error: tweetsError } = await supabase
+    .from(getTableName('tweets'))
+    .select('id', { count: 'exact', head: true })
+
+  if (accountsError || tweetsError) {
+    console.error('Error fetching stats:', accountsError || tweetsError)
+    throw accountsError || tweetsError
   }
 
-  return accounts.map((account: any) => account.username)
+  return {
+    usernames: accounts.map((account: any) => account.username),
+    accountCount: accounts.length,
+    tweetCount,
+  }
 }
 
 const CommunityStats = () => {
-  const [usernames, setUsernames] = useState([])
+  const [stats, setStats] = useState({
+    usernames: [],
+    accountCount: 0,
+    tweetCount: 0,
+  })
 
   useEffect(() => {
-    const fetchUsernames = async () => {
+    const fetchStats = async () => {
       const supabase = createBrowserClient()
-      const usernames = await getUsernames(supabase)
-      setUsernames(usernames || [])
+      const fetchedStats = await getStats(supabase)
+      setStats(fetchedStats)
     }
 
-    fetchUsernames()
+    fetchStats()
   }, [])
 
   return (
     <div>
       <p className="mb-4 text-sm">
+        <strong>{stats.accountCount}</strong> accounts have uploaded a total of{' '}
+        <strong>{stats.tweetCount}</strong> tweets.
+      </p>
+      <p className="mb-4 text-sm">
         These wonderful people have already uploaded their archives:
       </p>
       <div className="mb-8 max-h-40 overflow-y-auto">
-        {usernames.map((username) => (
+        {stats.usernames.map((username) => (
           <div key={username} className="mb-2">
             {username}
           </div>

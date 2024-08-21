85e40b55752e914d5fa5f69d486ad3617dd83f82
TheExGenesis <theexgenesis@gmail.com>
2024-08-21 23:17:41 +0100

    feat: add community tweets, likes, and handle user mentions better

--- scripts/dev_table_create.sql
+++ scripts/dev_table_create.sql
@@ -1,29 +1,27 @@
--- Create a function to apply restrictive RLS policies
-CREATE OR REPLACE FUNCTION apply_dev_rls_policies(table_name text)
-RETURNS void AS $$
-BEGIN
-    -- Enable RLS on the table
-    EXECUTE format('ALTER TABLE %I ENABLE ROW LEVEL SECURITY', table_name);
-    
-    -- Create a policy that only allows access (read and write) when using the service role
-    EXECUTE format('CREATE POLICY "dev tables can be read and written to by anyone" ON %I FOR ALL USING  (true)', table_name);
-END;
-$$ LANGUAGE plpgsql;
+GRANT USAGE ON SCHEMA dev TO anon, authenticated, service_role;
+GRANT ALL ON ALL TABLES IN SCHEMA dev TO anon, authenticated, service_role;
+GRANT ALL ON ALL ROUTINES IN SCHEMA dev TO anon, authenticated, service_role;
+GRANT ALL ON ALL SEQUENCES IN SCHEMA dev TO anon, authenticated, service_role;
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA dev GRANT ALL ON TABLES TO anon, authenticated, service_role;
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA dev GRANT ALL ON ROUTINES TO anon, authenticated, service_role;
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA dev GRANT ALL ON SEQUENCES TO anon, authenticated, service_role;
 
--- Example usage:
--- SELECT apply_dev_rls_policies('dev_table1');
--- SELECT apply_dev_rls_policies('dev_table2');
-
-DROP TABLE IF EXISTS dev_profile CASCADE;
-DROP TABLE IF EXISTS dev_following CASCADE;
-DROP TABLE IF EXISTS dev_followers CASCADE;
-DROP TABLE IF EXISTS dev_tweet_media CASCADE;
-DROP TABLE IF EXISTS dev_tweet_entities CASCADE;
-DROP TABLE IF EXISTS dev_tweets CASCADE;
-DROP TABLE IF EXISTS dev_account CASCADE;
-DROP TABLE IF EXISTS dev_entities CASCADE;
-DROP TABLE IF EXISTS dev_media CASCADE;
-DROP TABLE IF EXISTS dev_users CASCADE;
+DROP TABLE IF EXISTS dev.archive_upload CASCADE;
+DROP TABLE IF EXISTS dev.profile CASCADE;
+DROP TABLE IF EXISTS dev.following CASCADE;
+DROP TABLE IF EXISTS dev.followers CASCADE;
+DROP TABLE IF EXISTS dev.tweet_media CASCADE;
+DROP TABLE IF EXISTS dev.tweets CASCADE;
+DROP TABLE IF EXISTS dev.likes CASCADE;
+DROP TABLE IF EXISTS dev.liked_tweets CASCADE;
+DROP TABLE IF EXISTS dev.account CASCADE;
+DROP TABLE IF EXISTS dev.entities CASCADE;
+DROP TABLE IF EXISTS dev.media CASCADE;
+DROP TABLE IF EXISTS dev.users CASCADE;
+DROP TABLE IF EXISTS dev.user_mentions CASCADE;
+DROP TABLE IF EXISTS dev.mentioned_users CASCADE;
+DROP TABLE IF EXISTS dev.tweet_urls CASCADE;
+DROP TABLE IF EXISTS dev.tweet_entities CASCADE;
 
 
 -- Supabase AI is experimental and may produce incorrect answers
@@ -33,125 +31,261 @@ DROP TABLE IF EXISTS dev_users CASCADE;
 -- Enable UUID extension (if not already enabled)
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
+
+
 -- Create table for account information
 CREATE TABLE
-  dev_account (
-    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    -- email TEXT,
+  dev.account (
+    account_id TEXT PRIMARY KEY,
     created_via TEXT,
     username TEXT,
-    account_id TEXT UNIQUE,
-    archive_at TIMESTAMP WITH TIME ZONE,
+    -- account_id TEXT UNIQUE,
     created_at TIMESTAMP WITH TIME ZONE,
     account_display_name TEXT
   );
 
+-- Create table for profiles
+CREATE TABLE
+  dev.archive_upload (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    account_id TEXT,
+    archive_at TIMESTAMP WITH TIME ZONE,
+    UNIQUE (account_id, archive_at),
+    FOREIGN KEY (account_id) REFERENCES dev.account (account_id)
+  );
 
 -- Create table for profiles
 CREATE TABLE
-  dev_profile (
+  dev.profile (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    account_id TEXT UNIQUE,
+    account_id TEXT UNIQUE, 
     bio TEXT,
     website TEXT,
     LOCATION TEXT,
     avatar_media_url TEXT,
     header_media_url TEXT,
-    archive_at TIMESTAMP WITH TIME ZONE,
-    FOREIGN KEY (account_id) REFERENCES dev_account (account_id)
+    archive_upload_id BIGINT,
+    FOREIGN KEY (archive_upload_id) REFERENCES dev.archive_upload (id),
+    FOREIGN KEY (account_id) REFERENCES dev.account (account_id)
   );
-
--- Create a view to show the latest profile for each account_id
-CREATE VIEW
-  latest_dev_profile AS
-SELECT
-  dp.*
-FROM
-  dev_profile dp
-  JOIN (
-    SELECT
-      account_id,
-      MAX(archive_at) AS latest_archive_at
-    FROM
-      dev_profile
-    GROUP BY
-      account_id
-  ) latest ON dp.account_id = latest.account_id
-  AND dp.archive_at = latest.latest_archive_at;
 
 -- Create table for tweets
 CREATE TABLE
-  dev_tweets (
-    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    tweet_id TEXT UNIQUE,
+  dev.tweets (
+    tweet_id TEXT PRIMARY KEY,
     account_id TEXT,
     created_at TIMESTAMP WITH TIME ZONE,
     full_text TEXT,
-    -- lang TEXT,
     retweet_count INTEGER,
     favorite_count INTEGER,
     reply_to_tweet_id TEXT,
     reply_to_user_id TEXT,
     reply_to_username TEXT,
-    is_retweet BOOLEAN,
-    FOREIGN KEY (account_id) REFERENCES dev_account (account_id)
+    archive_upload_id BIGINT,
+    FOREIGN KEY (archive_upload_id) REFERENCES dev.archive_upload (id),
+    FOREIGN KEY (account_id) REFERENCES dev.account (account_id)
   );
 
--- Create table for tweet entities
+-- Create table for mentioned users
 CREATE TABLE
-  dev_tweet_entities (
+  dev.mentioned_users (
+    user_id TEXT PRIMARY KEY,
+    name TEXT,
+    screen_name TEXT,
+    updated_at TIMESTAMP WITH TIME ZONE
+  ) ;
+
+CREATE TABLE
+  dev.user_mentions (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    mentioned_user_id TEXT,
     tweet_id TEXT,
-    entity_type TEXT,
-    entity_value TEXT,
-    position_index INTEGER,
-    start_index INTEGER,
-    end_index INTEGER,
-    FOREIGN KEY (tweet_id) REFERENCES dev_tweets (tweet_id),
-    UNIQUE (tweet_id, entity_type, position_index)
+    foreign key (tweet_id) references dev.tweets (tweet_id),
+    foreign key (mentioned_user_id) references dev.mentioned_users (user_id),
+    unique(mentioned_user_id, tweet_id)
   );
+
+-- Create table for URLs
+CREATE TABLE
+  dev.tweet_urls (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    url TEXT,
+    expanded_url TEXT,
+    display_url TEXT,
+    tweet_id TEXT,
+    foreign key (tweet_id) references dev.tweets (tweet_id),
+    unique(tweet_id, url)
+  );
+
+
 
 -- Create table for tweet media
 CREATE TABLE
-  dev_tweet_media (
-    media_id TEXT PRIMARY KEY,
+  dev.tweet_media (
+    media_id BIGINT PRIMARY KEY,
     tweet_id TEXT,
     media_url TEXT,
     media_type TEXT,
     WIDTH INTEGER,
     HEIGHT INTEGER,
-    FOREIGN KEY (tweet_id) REFERENCES dev_tweets (tweet_id)
+    archive_upload_id BIGINT,
+    FOREIGN KEY (archive_upload_id) REFERENCES dev.archive_upload (id),
+    FOREIGN KEY (tweet_id) REFERENCES dev.tweets (tweet_id)
   );
 
 -- Create table for followers
 CREATE TABLE
-  dev_followers (
+  dev.followers (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     account_id TEXT,
     follower_account_id TEXT,
+    archive_upload_id BIGINT,
     UNIQUE (account_id, follower_account_id),
-    FOREIGN KEY (account_id) REFERENCES dev_account (account_id)
+    FOREIGN KEY (account_id) REFERENCES dev.account (account_id),
+    FOREIGN KEY (archive_upload_id) REFERENCES dev.archive_upload (id)
   );
 
 -- Create table for following
 CREATE TABLE
-  dev_following (
+  dev.following (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     account_id TEXT,
     following_account_id TEXT,
+    archive_upload_id BIGINT,
     UNIQUE (account_id, following_account_id),
-    FOREIGN KEY (account_id) REFERENCES dev_account (account_id)
+    FOREIGN KEY (account_id) REFERENCES dev.account (account_id),
+    FOREIGN KEY (archive_upload_id) REFERENCES dev.archive_upload (id)
+  );
+
+CREATE TABLE
+  dev.liked_tweets (
+    tweet_id TEXT PRIMARY KEY,
+    full_text TEXT
+  );
+
+-- Create table for likes
+CREATE TABLE
+  dev.likes (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    account_id TEXT,
+    liked_tweet_id TEXT,
+    archive_upload_id BIGINT,
+    UNIQUE (account_id, liked_tweet_id),
+    FOREIGN KEY (account_id) REFERENCES dev.account (account_id),
+    FOREIGN KEY (liked_tweet_id) REFERENCES dev.liked_tweets (tweet_id),
+    FOREIGN KEY (archive_upload_id) REFERENCES dev.archive_upload (id)
   );
 
 
+--- PERMISSIONS
+-- Function to apply RLS policies for most tables
+CREATE OR REPLACE FUNCTION dev.apply_dev_rls_policies(schema_name TEXT, table_name TEXT) RETURNS void AS $$
+DECLARE
+    policy_name TEXT;
+    full_table_name TEXT;
+BEGIN
+    full_table_name := schema_name || '.' || table_name;
 
+    -- Enable RLS on the table
+    EXECUTE format('ALTER TABLE %I.%I ENABLE ROW LEVEL SECURITY', schema_name, table_name);
+
+    -- Drop existing policies
+    FOR policy_name IN (
+        SELECT policyname
+        FROM pg_policies
+        WHERE schemaname = schema_name AND tablename = table_name
+    ) LOOP
+        EXECUTE format('DROP POLICY IF EXISTS %I ON %I.%I', policy_name, schema_name, table_name);
+    END LOOP; 
+
+    EXECUTE format('CREATE POLICY "Tweets are publicly visible" ON %I.%I FOR SELECT USING (true)', schema_name, table_name);
+    EXECUTE format('CREATE POLICY "Tweets are modifiable by their users" ON %I.%I FOR ALL USING (account_id = auth.jwt() -> ''app_metadata'' ->> ''provider_id'') WITH CHECK (account_id = auth.jwt() -> ''app_metadata'' ->> ''provider_id'')', schema_name, table_name);
+END;
+$$ LANGUAGE plpgsql;
+
+-- Function to apply RLS policies for entity tables
+CREATE OR REPLACE FUNCTION dev.apply_dev_entities_rls_policies(schema_name TEXT, table_name TEXT) RETURNS void AS $$
+DECLARE
+    policy_name TEXT;
+    full_table_name TEXT;
+BEGIN
+    full_table_name := schema_name || '.' || table_name;
+
+    -- Enable RLS on the table
+    EXECUTE format('ALTER TABLE %I.%I ENABLE ROW LEVEL SECURITY', schema_name, table_name);
+
+    -- Drop existing policies
+    FOR policy_name IN (
+        SELECT policyname
+        FROM pg_policies
+        WHERE schemaname = schema_name AND tablename = table_name
+    ) LOOP
+        EXECUTE format('DROP POLICY IF EXISTS %I ON %I.%I', policy_name, schema_name, table_name);
+    END LOOP;
+
+    EXECUTE format('CREATE POLICY "Entities are publicly visible" ON %I.%I FOR SELECT USING (true)', schema_name, table_name);
+    EXECUTE format('CREATE POLICY "Entities are modifiable by their users" ON %I.%I FOR ALL USING (EXISTS (SELECT 1 FROM dev.tweets dt WHERE dt.account_id = auth.jwt() -> ''app_metadata'' ->> ''provider_id'' AND dt.tweet_id = %I.%I.tweet_id)) WITH CHECK (EXISTS (SELECT 1 FROM dev.tweets dt WHERE dt.account_id = auth.jwt() -> ''app_metadata'' ->> ''provider_id'' AND dt.tweet_id = %I.%I.tweet_id))', schema_name, table_name, schema_name, table_name, schema_name, table_name);
+END;
+$$ LANGUAGE plpgsql;
+
+-- Function to apply RLS policies for entity tables
+CREATE OR REPLACE FUNCTION dev.apply_dev_liked_tweets_rls_policies(schema_name TEXT, table_name TEXT) RETURNS void AS $$
+DECLARE
+    policy_name TEXT;
+    full_table_name TEXT;
+BEGIN
+    full_table_name := schema_name || '.' || table_name;
+
+    -- Enable RLS on the table
+    EXECUTE format('ALTER TABLE %I.%I ENABLE ROW LEVEL SECURITY', schema_name, table_name);
+
+    -- Drop existing policies
+    FOR policy_name IN (
+        SELECT policyname
+        FROM pg_policies
+        WHERE schemaname = schema_name AND tablename = table_name
+    ) LOOP
+        EXECUTE format('DROP POLICY IF EXISTS %I ON %I.%I', policy_name, schema_name, table_name);
+    END LOOP;
+
+    EXECUTE format('CREATE POLICY "Entities are publicly visible" ON %I.%I FOR SELECT USING (true)', schema_name, table_name);
+    EXECUTE format('CREATE POLICY "Entities are modifiable by their users" ON %I.%I FOR ALL USING (EXISTS (SELECT 1 FROM dev.account dt WHERE dt.account_id = auth.jwt() -> ''app_metadata'' ->> ''provider_id'')) WITH CHECK (EXISTS (SELECT 1 FROM dev.account dt WHERE dt.account_id = auth.jwt() -> ''app_metadata'' ->> ''provider_id''))', schema_name, table_name, schema_name, table_name, schema_name, table_name);
+END;
+$$ LANGUAGE plpgsql;
+
+-- Script to apply policies to multiple tables
 DO $$
 DECLARE
-    tables text[] := ARRAY['dev_profile', 'dev_account', 'dev_tweets', 'dev_tweet_entities', 'dev_tweet_media', 'dev_followers', 'dev_following'];
-    t text;
+    tables text[][] := ARRAY[
+        ARRAY['dev', 'profile'],
+        ARRAY['dev', 'archive_upload'],
+        ARRAY['dev', 'account'],
+        ARRAY['dev', 'tweets'],
+        ARRAY['dev', 'likes'],
+        ARRAY['dev', 'followers'],
+        ARRAY['dev', 'following']
+    ];
+    t text[];
 BEGIN
-    FOREACH t IN ARRAY tables
+    FOREACH t SLICE 1 IN ARRAY tables
     LOOP
-        PERFORM apply_dev_rls_policies(t);
+        PERFORM dev.apply_dev_rls_policies(t[1], t[2]);
     END LOOP;
 END $$;
+
+-- Apply entity policies
+SELECT dev.apply_dev_entities_rls_policies('dev', 'tweet_media');
+SELECT dev.apply_dev_entities_rls_policies('dev', 'tweet_urls');
+SELECT dev.apply_dev_entities_rls_policies('dev', 'user_mentions');
+SELECT dev.apply_dev_liked_tweets_rls_policies('dev', 'liked_tweets');
+SELECT dev.apply_dev_liked_tweets_rls_policies('dev', 'mentioned_users');
+
+-- FTS index
+alter table dev.tweets
+drop column if exists fts;
+
+alter table dev.tweets
+add column fts tsvector generated always as (to_tsvector('english', full_text)) stored;
+
+
+create index text_fts on dev.tweets using gin (fts);
--- scripts/prod_table_create.sql
+++ scripts/prod_table_create.sql
@@ -1,155 +1,283 @@
--- Create a function to apply restrictive RLS policies
-CREATE OR REPLACE FUNCTION apply_dev_rls_policies(table_name text)
-RETURNS void AS $$
-BEGIN
-    -- Enable RLS on the table
-    EXECUTE format('ALTER TABLE %I ENABLE ROW LEVEL SECURITY', table_name);
-    
-    -- Create a policy that only allows access (read and write) when using the service role
-    EXECUTE format('CREATE POLICY "dev tables can be read and written to by anyone" ON %I FOR ALL USING  (true)', table_name);
-END;
-$$ LANGUAGE plpgsql;
+DROP TABLE IF EXISTS public.archive_upload CASCADE;
+DROP TABLE IF EXISTS public.profile CASCADE;
+DROP TABLE IF EXISTS public.following CASCADE;
+DROP TABLE IF EXISTS public.followers CASCADE;
+DROP TABLE IF EXISTS public.tweet_media CASCADE;
+DROP TABLE IF EXISTS public.tweets CASCADE;
+DROP TABLE IF EXISTS public.likes CASCADE;
+DROP TABLE IF EXISTS public.liked_tweets CASCADE;
+DROP TABLE IF EXISTS public.account CASCADE;
+DROP TABLE IF EXISTS public.entities CASCADE;
+DROP TABLE IF EXISTS public.media CASCADE;
+DROP TABLE IF EXISTS public.users CASCADE;
+DROP TABLE IF EXISTS public.user_mentions CASCADE;
+DROP TABLE IF EXISTS public.mentioned_users CASCADE;
+DROP TABLE IF EXISTS public.tweet_urls CASCADE;
+DROP TABLE IF EXISTS public.tweet_entities CASCADE;
 
--- Example usage:
--- SELECT apply_dev_rls_policies('dev_table1');
--- SELECT apply_dev_rls_policies('dev_table2');
 
 -- Supabase AI is experimental and may produce incorrect answers
 -- Always verify the output before executing
-DROP TABLE IF EXISTS profile CASCADE;
-DROP TABLE IF EXISTS following CASCADE;
-DROP TABLE IF EXISTS followers CASCADE;
-DROP TABLE IF EXISTS tweet_media CASCADE;
-DROP TABLE IF EXISTS tweet_entities CASCADE;
-DROP TABLE IF EXISTS tweets CASCADE;
-DROP TABLE IF EXISTS account CASCADE;
-DROP TABLE IF EXISTS entities CASCADE;
-DROP TABLE IF EXISTS media CASCADE;
-DROP TABLE IF EXISTS users CASCADE;
+
 
 -- Enable UUID extension (if not already enabled)
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
+
+
 -- Create table for account information
 CREATE TABLE
-  account (
-    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    -- email TEXT,
+  public.account (
+    account_id TEXT PRIMARY KEY,
     created_via TEXT,
     username TEXT,
-    account_id TEXT UNIQUE,
-    archive_at TIMESTAMP WITH TIME ZONE,
+    -- account_id TEXT UNIQUE,
     created_at TIMESTAMP WITH TIME ZONE,
     account_display_name TEXT
   );
 
+-- Create table for profiles
+CREATE TABLE
+  public.archive_upload (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    account_id TEXT,
+    archive_at TIMESTAMP WITH TIME ZONE,
+    UNIQUE (account_id, archive_at),
+    FOREIGN KEY (account_id) REFERENCES public.account (account_id)
+  );
 
 -- Create table for profiles
 CREATE TABLE
-  profile (
+  public.profile (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    account_id TEXT UNIQUE,
+    account_id TEXT UNIQUE, 
     bio TEXT,
     website TEXT,
     LOCATION TEXT,
     avatar_media_url TEXT,
     header_media_url TEXT,
-    archive_at TIMESTAMP WITH TIME ZONE,
-    FOREIGN KEY (account_id) REFERENCES account (account_id)
+    archive_upload_id BIGINT,
+    FOREIGN KEY (archive_upload_id) REFERENCES public.archive_upload (id),
+    FOREIGN KEY (account_id) REFERENCES public.account (account_id)
   );
-
--- Create a view to show the latest profile for each account_id
-CREATE VIEW
-  latest_profile AS
-SELECT
-  dp.*
-FROM
-  profile dp
-  JOIN (
-    SELECT
-      account_id,
-      MAX(archive_at) AS latest_archive_at
-    FROM
-      profile
-    GROUP BY
-      account_id
-  ) latest ON dp.account_id = latest.account_id
-  AND dp.archive_at = latest.latest_archive_at;
 
 -- Create table for tweets
 CREATE TABLE
-  tweets (
-    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    tweet_id TEXT UNIQUE,
+  public.tweets (
+    tweet_id TEXT PRIMARY KEY,
     account_id TEXT,
     created_at TIMESTAMP WITH TIME ZONE,
     full_text TEXT,
-    -- lang TEXT,
     retweet_count INTEGER,
     favorite_count INTEGER,
     reply_to_tweet_id TEXT,
     reply_to_user_id TEXT,
     reply_to_username TEXT,
-    is_retweet BOOLEAN,
-    -- source TEXT,
-    -- possibly_sensitive BOOLEAN,
-    FOREIGN KEY (account_id) REFERENCES account (account_id)
+    archive_upload_id BIGINT,
+    FOREIGN KEY (archive_upload_id) REFERENCES public.archive_upload (id),
+    FOREIGN KEY (account_id) REFERENCES public.account (account_id)
   );
 
--- Create table for tweet entities
+-- Create table for mentioned users
 CREATE TABLE
-  tweet_entities (
+  public.mentioned_users (
+    user_id TEXT PRIMARY KEY,
+    name TEXT,
+    screen_name TEXT,
+    updated_at TIMESTAMP WITH TIME ZONE
+  ) ;
+
+CREATE TABLE
+  public.user_mentions (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    mentioned_user_id TEXT,
     tweet_id TEXT,
-    entity_type TEXT,
-    entity_value TEXT,
-    position_index INTEGER,
-    start_index INTEGER,
-    end_index INTEGER,
-    FOREIGN KEY (tweet_id) REFERENCES tweets (tweet_id),
-    UNIQUE (tweet_id, entity_type, position_index)
+    foreign key (tweet_id) references public.tweets (tweet_id),
+    foreign key (mentioned_user_id) references public.mentioned_users (user_id),
+    unique(mentioned_user_id, tweet_id)
   );
+
+-- Create table for URLs
+CREATE TABLE
+  public.tweet_urls (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    url TEXT,
+    expanded_url TEXT,
+    display_url TEXT,
+    tweet_id TEXT,
+    foreign key (tweet_id) references public.tweets (tweet_id),
+    unique(tweet_id, url)
+  );
+
+
 
 -- Create table for tweet media
 CREATE TABLE
-  tweet_media (
-    media_id TEXT PRIMARY KEY,
+  public.tweet_media (
+    media_id BIGINT PRIMARY KEY,
     tweet_id TEXT,
     media_url TEXT,
     media_type TEXT,
     WIDTH INTEGER,
     HEIGHT INTEGER,
-    FOREIGN KEY (tweet_id) REFERENCES tweets (tweet_id)
+    archive_upload_id BIGINT,
+    FOREIGN KEY (archive_upload_id) REFERENCES public.archive_upload (id),
+    FOREIGN KEY (tweet_id) REFERENCES public.tweets (tweet_id)
   );
 
 -- Create table for followers
 CREATE TABLE
-  followers (
+  public.followers (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     account_id TEXT,
     follower_account_id TEXT,
+    archive_upload_id BIGINT,
     UNIQUE (account_id, follower_account_id),
-    FOREIGN KEY (account_id) REFERENCES account (account_id)
+    FOREIGN KEY (account_id) REFERENCES public.account (account_id),
+    FOREIGN KEY (archive_upload_id) REFERENCES public.archive_upload (id)
   );
 
 -- Create table for following
 CREATE TABLE
-  following (
+  public.following (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     account_id TEXT,
     following_account_id TEXT,
+    archive_upload_id BIGINT,
     UNIQUE (account_id, following_account_id),
-    FOREIGN KEY (account_id) REFERENCES account (account_id)
+    FOREIGN KEY (account_id) REFERENCES public.account (account_id),
+    FOREIGN KEY (archive_upload_id) REFERENCES public.archive_upload (id)
+  );
+
+CREATE TABLE
+  public.liked_tweets (
+    tweet_id TEXT PRIMARY KEY,
+    full_text TEXT
+  );
+
+-- Create table for likes
+CREATE TABLE
+  public.likes (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    account_id TEXT,
+    liked_tweet_id TEXT,
+    archive_upload_id BIGINT,
+    UNIQUE (account_id, liked_tweet_id),
+    FOREIGN KEY (account_id) REFERENCES public.account (account_id),
+    FOREIGN KEY (liked_tweet_id) REFERENCES public.liked_tweets (tweet_id),
+    FOREIGN KEY (archive_upload_id) REFERENCES public.archive_upload (id)
   );
 
 
+--- PERMISSIONS
+-- Function to apply RLS policies for most tables
+CREATE OR REPLACE FUNCTION public.apply_public_rls_policies(schema_name TEXT, table_name TEXT) RETURNS void AS $$
+DECLARE
+    policy_name TEXT;
+    full_table_name TEXT;
+BEGIN
+    full_table_name := schema_name || '.' || table_name;
+
+    -- Enable RLS on the table
+    EXECUTE format('ALTER TABLE %I.%I ENABLE ROW LEVEL SECURITY', schema_name, table_name);
+
+    -- Drop existing policies
+    FOR policy_name IN (
+        SELECT policyname
+        FROM pg_policies
+        WHERE schemaname = schema_name AND tablename = table_name
+    ) LOOP
+        EXECUTE format('DROP POLICY IF EXISTS %I ON %I.%I', policy_name, schema_name, table_name);
+    END LOOP; 
+
+    EXECUTE format('CREATE POLICY "Tweets are publicly visible" ON %I.%I FOR SELECT USING (true)', schema_name, table_name);
+    EXECUTE format('CREATE POLICY "Tweets are modifiable by their users" ON %I.%I FOR ALL USING (account_id = auth.jwt() -> ''app_metadata'' ->> ''provider_id'') WITH CHECK (account_id = auth.jwt() -> ''app_metadata'' ->> ''provider_id'')', schema_name, table_name);
+END;
+$$ LANGUAGE plpgsql;
+
+-- Function to apply RLS policies for entity tables
+CREATE OR REPLACE FUNCTION public.apply_public_entities_rls_policies(schema_name TEXT, table_name TEXT) RETURNS void AS $$
+DECLARE
+    policy_name TEXT;
+    full_table_name TEXT;
+BEGIN
+    full_table_name := schema_name || '.' || table_name;
+
+    -- Enable RLS on the table
+    EXECUTE format('ALTER TABLE %I.%I ENABLE ROW LEVEL SECURITY', schema_name, table_name);
+
+    -- Drop existing policies
+    FOR policy_name IN (
+        SELECT policyname
+        FROM pg_policies
+        WHERE schemaname = schema_name AND tablename = table_name
+    ) LOOP
+        EXECUTE format('DROP POLICY IF EXISTS %I ON %I.%I', policy_name, schema_name, table_name);
+    END LOOP;
+
+    EXECUTE format('CREATE POLICY "Entities are publicly visible" ON %I.%I FOR SELECT USING (true)', schema_name, table_name);
+    EXECUTE format('CREATE POLICY "Entities are modifiable by their users" ON %I.%I FOR ALL USING (EXISTS (SELECT 1 FROM public.tweets dt WHERE dt.account_id = auth.jwt() -> ''app_metadata'' ->> ''provider_id'' AND dt.tweet_id = %I.%I.tweet_id)) WITH CHECK (EXISTS (SELECT 1 FROM public.tweets dt WHERE dt.account_id = auth.jwt() -> ''app_metadata'' ->> ''provider_id'' AND dt.tweet_id = %I.%I.tweet_id))', schema_name, table_name, schema_name, table_name, schema_name, table_name);
+END;
+$$ LANGUAGE plpgsql;
+
+-- Function to apply RLS policies for entity tables
+CREATE OR REPLACE FUNCTION public.apply_public_liked_tweets_rls_policies(schema_name TEXT, table_name TEXT) RETURNS void AS $$
+DECLARE
+    policy_name TEXT;
+    full_table_name TEXT;
+BEGIN
+    full_table_name := schema_name || '.' || table_name;
+
+    -- Enable RLS on the table
+    EXECUTE format('ALTER TABLE %I.%I ENABLE ROW LEVEL SECURITY', schema_name, table_name);
+
+    -- Drop existing policies
+    FOR policy_name IN (
+        SELECT policyname
+        FROM pg_policies
+        WHERE schemaname = schema_name AND tablename = table_name
+    ) LOOP
+        EXECUTE format('DROP POLICY IF EXISTS %I ON %I.%I', policy_name, schema_name, table_name);
+    END LOOP;
+
+    EXECUTE format('CREATE POLICY "Entities are publicly visible" ON %I.%I FOR SELECT USING (true)', schema_name, table_name);
+    EXECUTE format('CREATE POLICY "Entities are modifiable by their users" ON %I.%I FOR ALL USING (EXISTS (SELECT 1 FROM public.account dt WHERE dt.account_id = auth.jwt() -> ''app_metadata'' ->> ''provider_id'')) WITH CHECK (EXISTS (SELECT 1 FROM public.account dt WHERE dt.account_id = auth.jwt() -> ''app_metadata'' ->> ''provider_id''))', schema_name, table_name, schema_name, table_name, schema_name, table_name);
+END;
+$$ LANGUAGE plpgsql;
+
+-- Script to apply policies to multiple tables
 DO $$
 DECLARE
-    tables text[] := ARRAY['profile', 'account', 'tweets', 'tweet_entities', 'tweet_media', 'followers', 'following'];
-    t text;
+    tables text[][] := ARRAY[
+        ARRAY['public', 'profile'],
+        ARRAY['public', 'archive_upload'],
+        ARRAY['public', 'account'],
+        ARRAY['public', 'tweets'],
+        ARRAY['public', 'likes'],
+        ARRAY['public', 'followers'],
+        ARRAY['public', 'following']
+    ];
+    t text[];
 BEGIN
-    FOREACH t IN ARRAY tables
+    FOREACH t SLICE 1 IN ARRAY tables
     LOOP
-        PERFORM apply_dev_rls_policies(t);
+        PERFORM public.apply_public_rls_policies(t[1], t[2]);
     END LOOP;
 END $$;
+
+-- Apply entity policies
+SELECT public.apply_public_entities_rls_policies('public', 'tweet_media');
+SELECT public.apply_public_entities_rls_policies('public', 'tweet_urls');
+SELECT public.apply_public_entities_rls_policies('public', 'user_mentions');
+SELECT public.apply_public_liked_tweets_rls_policies('public', 'liked_tweets');
+SELECT public.apply_public_liked_tweets_rls_policies('public', 'mentioned_users');
+
+-- FTS index
+alter table public.tweets
+drop column if exists fts;
+
+alter table public.tweets
+add column fts tsvector generated always as (to_tsvector('english', full_text)) stored;
+
+
+create index text_fts on public.tweets using gin (fts);

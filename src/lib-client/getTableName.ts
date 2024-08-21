cc9da5de9c7dc7511579e289d5e718b5d7a96751
TheExGenesis <theexgenesis@gmail.com>
2024-08-21 14:17:32 +0100

    docs: update db types

--- src/lib-client/getTableName.ts
+++ src/lib-client/getTableName.ts
@@ -2,11 +2,8 @@ import { Database } from '../database-types'
 
 const isProduction = process.env.NODE_ENV === 'production'
 
-type TableName = keyof Database['public']['Tables']
-type DevTableName = `dev_${TableName}`
-type AllTableNames = TableName | DevTableName
+type TableName =
+  | keyof Database['public']['Tables']
+  | keyof Database['dev']['Tables']
 
-export const getTableName = <T extends TableName>(
-  baseName: T,
-): T | `dev_${T}` =>
-  (isProduction ? baseName : `dev_${baseName}`) as T | `dev_${T}`
+export const getTableName = <T extends TableName>(baseName: T): T => baseName

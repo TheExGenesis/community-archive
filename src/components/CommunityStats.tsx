9c5b5c09b912431c438da7dd4348bcec71d1d110
TheExGenesis <theexgenesis@gmail.com>
2024-09-09 12:02:10 -0400

    fix: stats

--- src/components/CommunityStats.tsx
+++ src/components/CommunityStats.tsx
@@ -5,14 +5,16 @@ const CommunityStats = async () => {
 
   return (
     <div>
-      {stats.accountCount !== null && stats.tweetCount !== null && (
-        <p className="mb-4 text-sm">
-          <strong>{stats.accountCount}</strong> accounts have uploaded a total
-          of <strong>{stats.tweetCount}</strong> tweets. We also have{' '}
-          <strong>{stats.likedTweetCount}</strong> liked tweets.
-        </p>
-      )}
-      {stats.usernames.length > 0 && (
+      {stats.accountCount !== null &&
+        stats.tweetCount !== null &&
+        stats.likedTweetCount !== null && (
+          <p className="mb-4 text-sm">
+            <strong>{stats.accountCount}</strong> accounts have uploaded a total
+            of <strong>{stats.tweetCount}</strong> tweets. We also have{' '}
+            <strong>{stats.likedTweetCount}</strong> liked tweets.
+          </p>
+        )}
+      {stats.usernames && stats.usernames.length > 0 && (
         <p className="mb-4 text-sm">
           Accounts in the archive: {stats.usernames.join(', ')}
         </p>

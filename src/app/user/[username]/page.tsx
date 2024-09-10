5a86de14e8222203c2d5c46796544fc6adf03685
DefenderOfBasic <defenderofbasic@proton.me>
2024-09-10 12:35:10 -0400

    fix lint errors

--- src/app/user/[username]/page.tsx
+++ src/app/user/[username]/page.tsx
@@ -4,13 +4,18 @@ import Tweet from '@/components/TweetRefactor'
 
 export default async function User({ params, searchParams }:any) {
     const { username } = params
-    const { tweetCount, account } = await getUserData(username.toLowerCase())
+    const userData = await getUserData(username.toLowerCase())
+    if (!userData) {
+      return (<h1>Not found</h1>)
+    }
+
+    const { tweetCount, account } = userData
     const firstTweets = await getFirstTweets(account.account_id)
     const topTweets = await getTopTweets(account.account_id)
 
     return (
       <div id="user-page">
-        <h1>{username} ({new Intl.NumberFormat().format(tweetCount)} tweets)</h1>
+        <h1>{username} ({new Intl.NumberFormat().format(tweetCount as number)} tweets)</h1>
         
         <h2>Top 20 tweets</h2>
         <div className="short-tweet-container">

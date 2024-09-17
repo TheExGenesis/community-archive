40ec40e99bf5ea505c50df46a6b63f2fb364c512
TheExGenesis <theexgenesis@gmail.com>
2024-09-17 00:49:06 -0400

    feat: user dir

--- src/lib-server/user.ts
+++ src/lib-server/user.ts
@@ -13,55 +13,54 @@ const tweetSelectString = `
         )
       `
 
-export const getFirstTweets = async(account_id:string, limit:number= 100) => {
+export const getFirstTweets = async (
+  account_id: string,
+  limit: number = 100,
+) => {
   const supabase = createServerClient(cookies())
-  const { data } = await 
-    supabase
-      .schema(getSchemaName())
-      .from('tweets')
-      .select(tweetSelectString)
-      .eq('account_id', account_id)
-      .order('created_at', { ascending: true })
-      .limit(limit)
+  const { data } = await supabase
+    .schema('public')
+    .from('tweets')
+    .select(tweetSelectString)
+    .eq('account_id', account_id)
+    .order('created_at', { ascending: true })
+    .limit(limit)
   return data
 }
 
-export const getTopTweets = async(account_id:string, limit:number= 20) => {
+export const getTopTweets = async (account_id: string, limit: number = 20) => {
   const supabase = createServerClient(cookies())
-  const { data } = await 
-    supabase
-      .schema(getSchemaName())
-      .from('tweets')
-      .select(tweetSelectString)
-      .eq('account_id', account_id)
-      .order('retweet_count', { ascending: false })
-      .order('favorite_count', { ascending: false })
-      .limit(limit)
+  const { data } = await supabase
+    .schema('public')
+    .from('tweets')
+    .select(tweetSelectString)
+    .eq('account_id', account_id)
+    .order('retweet_count', { ascending: false })
+    .order('favorite_count', { ascending: false })
+    .limit(limit)
   return data
 }
 
-export const getUserData = async (account_id:string) => {
+export const getUserData = async (account_id: string) => {
   const supabase = createServerClient(cookies())
-  const { data } = await 
-    supabase
-      .schema(getSchemaName())
-      .from('account')
-      .select('*')
-      .eq('account_id', account_id)
+  const { data } = await supabase
+    .schema('public')
+    .from('account')
+    .select('*')
+    .eq('account_id', account_id)
 
   if (!data || data.length == 0) {
     return null
   }
 
+  const { count } = await supabase
+    .schema('public')
+    .from('tweets')
+    .select('tweet_id', { count: 'planned', head: true })
+    .eq('account_id', account_id)
 
-    const { count } = await supabase
-      .schema(getSchemaName())
-      .from('tweets')
-      .select('tweet_id', { count: 'planned', head: true })
-      .eq('account_id', account_id)
-
-    return {
-        account: data[0],
-        tweetCount: count
-    }
+  return {
+    account: data[0],
+    tweetCount: count,
+  }
 }

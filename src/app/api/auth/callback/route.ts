7b86c7416c341a7554cfc543dc6f93aeeea7749c
TheExGenesis <theexgenesis@gmail.com>
2024-09-26 16:09:41 +0100

    refactor: auth supabase client

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -1,49 +1,13 @@
 import { NextResponse } from 'next/server'
-import { createServerClient } from '@/utils/supabase'
+import { createServerAdminClient } from '@/utils/supabase'
 import { cookies } from 'next/headers'
-import { Database } from '@/database-types'
-import {
-  createServerClient as serverClient,
-  type CookieOptions,
-} from '@supabase/ssr'
-
-export const createServerAdminClient = (
-  cookieStore: ReturnType<typeof cookies>,
-) =>
-  serverClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.SUPABASE_SERVICE_ROLE!,
-    {
-      cookies: {
-        get(name: string) {
-          return cookieStore.get(name)?.value
-        },
-        set(name: string, value: string, options: CookieOptions) {
-          try {
-            cookieStore.set({ name, value, ...options })
-          } catch (error) {
-            // The `set` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
-          }
-        },
-        remove(name: string, options: CookieOptions) {
-          try {
-            cookieStore.set({ name, value: '', ...options })
-          } catch (error) {
-            // The `delete` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
-          }
-        },
-      },
-    },
-  )
 
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url)
   const code = searchParams.get('code')
   const next = searchParams.get('next') ?? '/'
+
+  // console.log({ code, next, searchParams, origin, request })
 
   if (code) {
     const supabase = createServerAdminClient(cookies())

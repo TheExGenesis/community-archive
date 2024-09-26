a87139173c1ea663ec9c7fdbc2486c984bb00b19
TheExGenesis <theexgenesis@gmail.com>
2024-09-26 16:09:41 +0100

    refactor: auth supabase client

--- src/lib-server/db_queries.ts
+++ src/lib-server/db_queries.ts
@@ -2,7 +2,6 @@ import { getSchemaName } from '@/lib-client/getTableName'
 
 export const getTweetsCount = async (supabase: any, account_id: string) => {
   return await supabase
-    // .schema(getSchemaName())
     .from('account')
     .select('num_tweets', { count: 'planned' })
     .eq('account_id', account_id)

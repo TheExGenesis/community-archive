bb5d841e5c8465768c3824383b6cbfe62d01379d
TheExGenesis <theexgenesis@gmail.com>
2024-10-04 14:47:22 +0100

    db: refactor apply_policies

--- sql/setup/02_apply_policies.sql
+++ sql/setup/02_apply_policies.sql
@@ -1,28 +1,29 @@
--- Apply RLS policies to all relevant tables
-
+-- Drop all policies and apply new ones for each table
 DO $$
-DECLARE
-    tables text[][] := ARRAY[
-        ARRAY['public', 'profile'],
-        ARRAY['public', 'archive_upload'],
-        ARRAY['public', 'account'],
-        ARRAY['public', 'tweets'],
-        ARRAY['public', 'likes'],
-        ARRAY['public', 'followers'],
-        ARRAY['public', 'following']
-    ];
-    t text[];
 BEGIN
-    FOREACH t SLICE 1 IN ARRAY tables
-    LOOP
-        PERFORM public.apply_public_rls_policies(t[1], t[2]);
-    END LOOP;
+    -- Drop all policies
+    PERFORM public.drop_all_policies('public', 'account');
+    PERFORM public.drop_all_policies('public', 'archive_upload');
+    PERFORM public.drop_all_policies('public', 'profile');
+    PERFORM public.drop_all_policies('public', 'tweets');
+    PERFORM public.drop_all_policies('public', 'likes');
+    PERFORM public.drop_all_policies('public', 'followers');
+    PERFORM public.drop_all_policies('public', 'following');
+    PERFORM public.drop_all_policies('public', 'tweet_media');
+    PERFORM public.drop_all_policies('public', 'tweet_urls');
+    PERFORM public.drop_all_policies('public', 'user_mentions');
+
+    -- Apply new policies
+    PERFORM public.apply_public_rls_policies_not_private('public', 'account');
+    PERFORM public.apply_public_rls_policies_not_private('public', 'archive_upload');
+    PERFORM public.apply_public_rls_policies_not_private('public', 'profile');
+    PERFORM public.apply_public_rls_policies('public', 'tweets');
+    PERFORM public.apply_public_rls_policies('public', 'likes');
+    PERFORM public.apply_public_rls_policies('public', 'followers');
+    PERFORM public.apply_public_rls_policies('public', 'following');
+
+    -- Apply entity policies
+    PERFORM public.apply_public_entities_rls_policies('public', 'tweet_media');
+    PERFORM public.apply_public_entities_rls_policies('public', 'tweet_urls');
+    PERFORM public.apply_public_entities_rls_policies('public', 'user_mentions');
 END $$;
-
--- Apply entity policies
-SELECT public.apply_public_entities_rls_policies('public', 'tweet_media');
-SELECT public.apply_public_entities_rls_policies('public', 'tweet_urls');
-SELECT public.apply_public_entities_rls_policies('public', 'user_mentions');
-
-SELECT public.apply_public_liked_tweets_rls_policies('public', 'liked_tweets');
-SELECT public.apply_public_liked_tweets_rls_policies('public', 'mentioned_users');

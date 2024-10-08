97eb91e459840291e680336d4561c791886c2786
TheExGenesis <theexgenesis@gmail.com>
2024-10-08 22:43:45 +0100

    db: migrations

--- supabase/migrations/20241006195507_top_lists_fns.sql
+++ supabase/migrations/20241006195507_top_lists_fns.sql
@@ -1,4 +1,5 @@
 -- Function 4: get_top_mentioned_users_not_uploaded
+DROP FUNCTION IF EXISTS public.get_top_mentioned_users_not_uploaded();
 CREATE OR REPLACE FUNCTION public.get_top_mentioned_users_not_uploaded()
 RETURNS TABLE (
     mentioned_user_id TEXT,
@@ -38,6 +39,7 @@ END;
 $$ LANGUAGE plpgsql;
 
 -- Function 5: get_account_most_liked_tweets_archive_users
+DROP FUNCTION IF EXISTS public.get_account_most_liked_tweets_archive_users(TEXT, INTEGER);
 CREATE OR REPLACE FUNCTION public.get_account_most_liked_tweets_archive_users(
     username_ TEXT,
     limit_ INTEGER DEFAULT NULL
@@ -86,6 +88,7 @@ END;
 $$ LANGUAGE plpgsql;
 
 -- Function 6: get_account_most_replied_tweets_by_archive_users
+DROP FUNCTION IF EXISTS public.get_account_most_replied_tweets_by_archive_users(TEXT, INTEGER);
 CREATE OR REPLACE FUNCTION public.get_account_most_replied_tweets_by_archive_users (
     username_ TEXT, 
     limit_count INTEGER
@@ -135,6 +138,7 @@ END;
 $$ LANGUAGE plpgsql;
 
 -- Function 7: get_account_top_retweet_count_tweets
+DROP FUNCTION IF EXISTS public.get_account_top_retweet_count_tweets(TEXT, INTEGER);
 CREATE OR REPLACE FUNCTION public.get_account_top_retweet_count_tweets (
     username_ TEXT, 
     limit_ INTEGER
@@ -173,58 +177,5 @@ BEGIN
         t.retweet_count DESC 
     LIMIT 
         limit_;
-END;
-$$ LANGUAGE plpgsql;
-
--- Function 8: get_account_most_mentioned_accounts
-CREATE OR REPLACE FUNCTION public.get_account_most_mentioned_accounts (
-    username_ TEXT, 
-    limit_ INTEGER
-) RETURNS TABLE (
-  mentioned_user_id TEXT,
-  mentioned_username TEXT,
-  mention_count BIGINT
-) AS $$
-DECLARE
-    user_id text;
-BEGIN
-    -- Get the user_id based on the provided username
-    SELECT account_id INTO user_id
-    FROM public.account
-    WHERE username = username_;
-
-    -- If the user_id is not found, return an empty result
-    IF user_id IS NULL THEN
-        RETURN;
-    END IF;
-
-    RETURN QUERY
-    WITH TopMentionedUsers AS (
-        SELECT
-            um.mentioned_user_id,
-            COUNT(*) AS mention_count
-        FROM
-            public.user_mentions um
-        JOIN
-            public.tweets t ON um.tweet_id = t.tweet_id
-        WHERE
-            t.account_id = user_id
-            AND um.mentioned_user_id <> '-1'
-        GROUP BY
-            um.mentioned_user_id
-        ORDER BY
-            mention_count DESC
-        LIMIT limit_
-    )
-    SELECT
-        t.mentioned_user_id,
-        mu.screen_name AS mentioned_username,
-        t.mention_count
-    FROM
-        TopMentionedUsers t
-    LEFT JOIN
-        public.mentioned_users mu ON t.mentioned_user_id = mu.user_id
-    ORDER BY
-        t.mention_count DESC;
 END;
 $$ LANGUAGE plpgsql;

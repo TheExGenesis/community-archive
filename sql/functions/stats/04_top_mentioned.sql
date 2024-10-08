5b772ebf6790a02f604d10142f7d96f3a93ba570
TheExGenesis <theexgenesis@gmail.com>
2024-10-08 11:20:19 +0100

    feat(most-mentioned): add most mentioned to mission control, and to user page

--- sql/functions/stats/04_top_mentioned.sql
+++ sql/functions/stats/04_top_mentioned.sql
@@ -1,4 +1,5 @@
-CREATE OR REPLACE FUNCTION public.get_top_mentioned_users_not_uploaded()
+
+CREATE OR REPLACE FUNCTION public.get_top_mentioned_users(limit_ INT)
 RETURNS TABLE (
     mentioned_user_id TEXT,
     name TEXT,
@@ -13,15 +14,17 @@ BEGIN
             COUNT(*) AS mention_count
         FROM
             public.user_mentions um
+        WHERE
+            um.mentioned_user_id <> '-1'
         GROUP BY
             um.mentioned_user_id
         ORDER BY
             mention_count DESC
         LIMIT
-            100
+            limit_
     )
     SELECT
-        t.mentioned_user_id,
+        t.mentioned_user_id as user_id,
         mu.name,
         mu.screen_name,
         t.mention_count
@@ -29,8 +32,6 @@ BEGIN
         TopMentionedUsers t
         JOIN public.mentioned_users mu ON t.mentioned_user_id = mu.user_id
         LEFT JOIN public.profile u ON t.mentioned_user_id = u.account_id
-    WHERE
-        u.id IS NULL
     ORDER BY
         t.mention_count DESC;
 END;

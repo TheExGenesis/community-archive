f75fae24509cf5958dfa2cf2a371f4c17e908950
TheExGenesis <theexgenesis@gmail.com>
2024-10-08 20:01:59 +0100

    feat: job queue to update activity summary

--- sql/views/materialized/01_global_activity_summary.sql
+++ sql/views/materialized/01_global_activity_summary.sql
@@ -27,16 +27,3 @@ SELECT
       SELECT * FROM public.get_top_accounts_with_followers(10)
     ) t
   ) AS top_accounts_with_followers;
-
-CREATE OR REPLACE FUNCTION refresh_global_activity_summary() RETURNS TRIGGER AS $$
-BEGIN
-    IF NEW.upload_phase = 'completed' THEN
-        REFRESH MATERIALIZED VIEW public.global_activity_summary;
-    END IF;
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE OR REPLACE TRIGGER update_global_activity_summary
-AFTER UPDATE OF upload_phase ON public.archive_upload
-FOR EACH ROW EXECUTE FUNCTION refresh_global_activity_summary();

16acef9f36e11e598406a3e90bd2a286e5e3a975
ri72miieop <106687171+ri72miieop@users.noreply.github.com>
2024-10-14 14:22:59 +0100

    Change schema of functions from public to private; Added index on main_thread_view Refresh materialized view concurrently

--- sql/functions/threads/02_post_upload_update_conversation_ids.sql
+++ sql/functions/threads/02_post_upload_update_conversation_ids.sql
@@ -1,12 +1,12 @@
-CREATE OR REPLACE FUNCTION public.post_upload_update_conversation_ids()
+CREATE OR REPLACE FUNCTION private.post_upload_update_conversation_ids()
 RETURNS void AS $$
 BEGIN
     
     RAISE NOTICE 'Updating conversation ids';
-    PERFORM public.update_conversation_ids();
+    PERFORM private.update_conversation_ids();
    
    
    RAISE NOTICE 'Refreshing materialized view: main_thread_view';
-    REFRESH MATERIALIZED VIEW main_thread_view;
+    REFRESH MATERIALIZED VIEW CONCURRENTLY main_thread_view;
 END;
 $$ LANGUAGE plpgsql;

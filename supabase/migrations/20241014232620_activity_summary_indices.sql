7aeb04bd558eccf772a4b519b33af6d4e135b949
TheExGenesis <theexgenesis@gmail.com>
2024-10-15 00:29:41 +0100

    fix: indices for activity summary

--- supabase/migrations/20241014232620_activity_summary_indices.sql
+++ supabase/migrations/20241014232620_activity_summary_indices.sql
@@ -1,3 +1,39 @@
+DROP MATERIALIZED VIEW IF EXISTS public.global_activity_summary;
+
+CREATE MATERIALIZED VIEW
+  public.global_activity_summary AS
+SELECT
+  (
+    SELECT COUNT(DISTINCT account_id) FROM public.account
+  ) AS total_accounts,
+  (
+    SELECT COUNT(DISTINCT tweet_id) FROM public.tweets
+  ) AS total_tweets,
+  (
+    SELECT COUNT(DISTINCT liked_tweet_id) FROM public.likes
+  ) AS total_likes,
+  (
+    SELECT COUNT(DISTINCT tweet_id) FROM public.user_mentions
+  ) AS total_user_mentions,
+  (
+    SELECT json_agg(row_to_json(t))
+    FROM (
+      SELECT * FROM public.get_top_mentioned_users(30)
+    ) t
+  ) AS top_mentioned_users,
+  (
+    SELECT json_agg(row_to_json(t))
+    FROM (
+      SELECT * FROM public.get_top_accounts_with_followers(10)
+    ) t
+  ) AS top_accounts_with_followers,
+  CURRENT_TIMESTAMP AS last_updated;
+
+-- Add a unique index on the last_updated column
+CREATE UNIQUE INDEX idx_global_activity_summary_last_updated 
+ON public.global_activity_summary (last_updated);
+
+
 DROP MATERIALIZED VIEW IF EXISTS public.account_activity_summary;
 
 CREATE MATERIALIZED VIEW public.account_activity_summary AS

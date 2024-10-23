28874fc905827b0817c352fbd49d101defc03efa
TheExGenesis <theexgenesis@gmail.com>
2024-10-23 11:46:26 +0100

    feat: rework login to redirect wherever

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -3,15 +3,39 @@ import { createServerAdminClient } from '@/utils/supabase'
 import { cookies } from 'next/headers'
 import { devLog } from '@/lib-client/devLog'
 
+// Add this function to validate URLs
+function isValidRedirectUrl(url: string): boolean {
+  try {
+    const parsedUrl = new URL(url)
+    const allowedDomains =
+      process.env.NEXT_ALLOWED_AUTH_REDIRECT_DOMAINS?.split(',').map(
+        (domain) => {
+          try {
+            return new URL(domain.trim()).hostname
+          } catch {
+            return domain.trim() // for 'localhost'
+          }
+        },
+      ) || []
+    return allowedDomains.includes(parsedUrl.hostname)
+  } catch {
+    return false
+  }
+}
+
 export async function GET(request: Request) {
+  console.log('request', request)
   const { searchParams, origin } = new URL(request.url)
   const code = searchParams.get('code')
   const next = searchParams.get('next') ?? '/'
+  const forwardedHost = request.headers.get('x-forwarded-host')
+
+  console.log('request.url', { url: request.url, code, next, origin })
 
   if (code) {
     const supabase = createServerAdminClient(cookies())
     const { data, error } = await supabase.auth.exchangeCodeForSession(code)
-    devLog({ data, error, next, origin, code })
+    devLog({ data, error, next, origin, code, forwardedHost })
     if (!error && data.user) {
       // Move provider_id to app_metadata and ensure username is lowercase
       const { data: updateData, error: updateError } =
@@ -26,20 +50,23 @@ export async function GET(request: Request) {
         console.error('Failed to update app_metadata:', updateError)
       }
 
-      const forwardedHost = request.headers.get('x-forwarded-host')
-      const isLocalEnv =
-        process.env.NODE_ENV === 'development' &&
-        !process.env.NEXT_PUBLIC_USE_REMOTE_DEV_DB
+      // Determine base URL
+      const baseUrl = forwardedHost ? `https://${forwardedHost}` : origin
 
-      if (isLocalEnv) {
-        return NextResponse.redirect(`${origin}${next}`)
-      } else if (forwardedHost) {
-        return NextResponse.redirect(`https://${forwardedHost}${next}`)
-      } else {
-        return NextResponse.redirect(`${origin}${next}`)
+      // Determine redirect URL
+      let redirectUrl = `${baseUrl}${next}`
+      if (isValidRedirectUrl(next)) {
+        redirectUrl = next
       }
+      console.log('redirectUrl', redirectUrl)
+
+      return NextResponse.redirect(redirectUrl)
     }
   }
 
-  return NextResponse.redirect(`${origin}/auth/auth-code-error`)
+  const errorRedirect = forwardedHost
+    ? `https://${forwardedHost}/auth/auth-code-error`
+    : `${origin}/auth/auth-code-error`
+
+  return NextResponse.redirect(errorRedirect)
 }

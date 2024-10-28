c0875f5ab3a7af04fa923981189c440f4f3cd5a7
TheExGenesis <theexgenesis@gmail.com>
2024-10-28 12:09:59 +0000

    fix: restore old callbakc route

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -3,53 +3,15 @@ import { createServerAdminClient } from '@/utils/supabase'
 import { cookies } from 'next/headers'
 import { devLog } from '@/lib-client/devLog'
 
-// Add this function to validate URLs
-function isValidRedirectUrl(url: string): boolean {
-  try {
-    const parsedUrl = new URL(url)
-    const allowedDomains =
-      process.env.NEXT_ALLOWED_AUTH_REDIRECT_DOMAINS?.split(',').map(
-        (domain) => {
-          try {
-            return new URL(domain.trim()).hostname
-          } catch {
-            return domain.trim() // for 'localhost'
-          }
-        },
-      ) || []
-    return allowedDomains.includes(parsedUrl.hostname)
-  } catch {
-    return false
-  }
-}
-
 export async function GET(request: Request) {
-  const requestUrl = new URL(request.url)
-  const { searchParams, origin } = requestUrl
+  const { searchParams, origin } = new URL(request.url)
   const code = searchParams.get('code')
   const next = searchParams.get('next') ?? '/'
-  const forwardedHost = request.headers.get('x-forwarded-host')
-
-  console.log('request.url', {
-    request,
-    url: request.url,
-    allParams: Object.fromEntries(requestUrl.searchParams),
-    headers: Object.fromEntries(request.headers),
-    state: requestUrl.searchParams.get('state'),
-    error: requestUrl.searchParams.get('error'),
-    errorDescription: requestUrl.searchParams.get('error_description'),
-    code,
-    next,
-    origin,
-    forwardedHost,
-    searchParams,
-    customUrlParam: searchParams.get('customUrlParam'),
-  })
 
   if (code) {
     const supabase = createServerAdminClient(cookies())
     const { data, error } = await supabase.auth.exchangeCodeForSession(code)
-    devLog({ data, error, next, origin, code, forwardedHost })
+    devLog({ data, error, next, origin, code })
     if (!error && data.user) {
       // Move provider_id to app_metadata and ensure username is lowercase
       const { data: updateData, error: updateError } =
@@ -64,24 +26,20 @@ export async function GET(request: Request) {
         console.error('Failed to update app_metadata:', updateError)
       }
 
-      // Determine base URL
-      // const baseUrl = forwardedHost ? `https://${forwardedHost}` : origin
-      const baseUrl = origin ? origin : `https://${forwardedHost}`
+      const forwardedHost = request.headers.get('x-forwarded-host')
+      const isLocalEnv =
+        process.env.NODE_ENV === 'development' &&
+        !process.env.NEXT_PUBLIC_USE_REMOTE_DEV_DB
 
-      // Determine redirect URL
-      let redirectUrl = `${baseUrl}${next}`
-      if (isValidRedirectUrl(next)) {
-        redirectUrl = next
+      if (isLocalEnv) {
+        return NextResponse.redirect(`${origin}${next}`)
+      } else if (forwardedHost) {
+        return NextResponse.redirect(`https://${forwardedHost}${next}`)
+      } else {
+        return NextResponse.redirect(`${origin}${next}`)
       }
-      console.log('redirectUrl', { redirectUrl, next, origin, forwardedHost })
-
-      return NextResponse.redirect(redirectUrl)
     }
   }
 
-  const errorRedirect = forwardedHost
-    ? `https://${forwardedHost}/auth/auth-code-error`
-    : `${origin}/auth/auth-code-error`
-
-  return NextResponse.redirect(errorRedirect)
+  return NextResponse.redirect(`${origin}/auth/auth-code-error`)
 }

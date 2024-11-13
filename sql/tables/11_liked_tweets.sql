202b82da5a311c2e9ebdd9606583657294349873
ri72miieop <106687171+ri72miieop@users.noreply.github.com>
2024-11-13 21:49:54 +0000

    update sql folder to be consistent with migration

--- sql/tables/11_liked_tweets.sql
+++ sql/tables/11_liked_tweets.sql
@@ -1,4 +1,7 @@
 CREATE TABLE IF NOT EXISTS public.liked_tweets (
     tweet_id TEXT PRIMARY KEY,
-    full_text TEXT NOT NULL
+    full_text TEXT NOT NULL,
+    fts tsvector GENERATED ALWAYS AS (to_tsvector('english', full_text)) STORED
 );
+
+CREATE INDEX IF NOT EXISTS text_fts ON public.liked_tweets USING gin (fts);

a4d8385bf0c049e117d8841b7ab0018ca32c5400
ri72miieop <106687171+ri72miieop@users.noreply.github.com>
2025-01-15 15:47:08 +0000

    updated delete_all_archives to use the new table names (all_profile and all_account)

--- supabase/migrations/20241204170257_TES_send_data_to_community_archive.sql
+++ supabase/migrations/20241204170257_TES_send_data_to_community_archive.sql
@@ -85,6 +85,7 @@ CREATE INDEX "idx_all_profile_archive_upload_id" ON "public"."all_profile" USING
  END;
  $$ LANGUAGE plpgsql;
  SELECT update_foreign_keys('account', 'all_account', 'public');
+ 
  /* ########### UPDATE OLD FUNCTIONS TO USE ALL_ACCOUNT ########### */
  -- Create or replace the commit_temp_data function
  CREATE OR REPLACE FUNCTION public.commit_temp_data(p_suffix TEXT)
@@ -273,7 +274,63 @@ CREATE INDEX "idx_all_profile_archive_upload_id" ON "public"."all_profile" USING
          RAISE;
  END;
  $$ LANGUAGE plpgsql SECURITY DEFINER;
+
+
+CREATE OR REPLACE FUNCTION public.delete_all_archives(p_account_id TEXT)
+RETURNS VOID AS $$
+DECLARE
+    v_schema_name TEXT := 'public';
+    v_archive_upload_ids BIGINT[];
+BEGIN
+    SELECT ARRAY_AGG(id) INTO v_archive_upload_ids
+    FROM public.archive_upload
+    WHERE account_id = p_account_id;
+
+    BEGIN
+        -- Delete tweets and related data
+        EXECUTE format('
+            WITH tweets_to_delete AS (
+                SELECT tweet_id FROM %I.tweets WHERE archive_upload_id = ANY($1) OR account_id = $2
+            )
+            DELETE FROM %I.tweet_media WHERE tweet_id IN (SELECT tweet_id FROM tweets_to_delete);
+
+                WITH tweets_to_delete AS (
+                    SELECT tweet_id FROM %I.tweets WHERE archive_upload_id = ANY($1) 
+                )
+            DELETE FROM %I.user_mentions WHERE tweet_id IN (SELECT tweet_id FROM tweets_to_delete);
+
+            WITH tweets_to_delete AS (
+                SELECT tweet_id FROM %I.tweets WHERE archive_upload_id = ANY($1) OR account_id = $2
+            )
+            DELETE FROM %I.tweet_urls WHERE tweet_id IN (SELECT tweet_id FROM tweets_to_delete);
+
+            DELETE FROM %I.tweets WHERE archive_upload_id = ANY($1);
+        ', v_schema_name, v_schema_name, v_schema_name, v_schema_name, v_schema_name, v_schema_name, v_schema_name)
+        USING v_archive_upload_ids, p_account_id;
+
+        -- Delete other related data
+        EXECUTE format('
+            DELETE FROM %I.likes WHERE archive_upload_id = ANY($1);
+            DELETE FROM %I.followers WHERE archive_upload_id = ANY($1);
+            DELETE FROM %I.following WHERE archive_upload_id = ANY($1);
+            DELETE FROM %I.all_profile WHERE archive_upload_id = ANY($1);
+            DELETE FROM %I.tweet_media WHERE archive_upload_id = ANY($1);
+            DELETE FROM %I.archive_upload WHERE id = ANY($1);
+            DELETE FROM %I.all_account WHERE account_id = $2;
+        ', v_schema_name, v_schema_name, v_schema_name, v_schema_name, v_schema_name, v_schema_name, v_schema_name)
+        USING v_archive_upload_ids, p_account_id;
+    EXCEPTION WHEN OTHERS THEN
+        RAISE NOTICE 'Error deleting archives for account %: %', p_account_id, SQLERRM;
+        RAISE;
+    END;
+END;
+$$ LANGUAGE plpgsql SECURITY DEFINER
+
+
+
  /* ########### UPDATE OLD FUNCTIONS TO USE ALL_ACCOUNT ########### */
+
+ 
  DO $$ 
  DECLARE
      account_count INTEGER;
@@ -1072,3 +1129,4 @@ CREATE POLICY temporary_data_no_delete_policy
  -- Schedule job to run every 5 minutes
  SELECT cron.schedule('tes-insert-temporary-data-into-tables', 
      '*/5 * * * *', $$SELECT private.tes_import_temporary_data_into_tables();$$);
+

26905ccd552e8eeba473a2a2c51a1bc6078a16de
ri72miieop <106687171+ri72miieop@users.noreply.github.com>
2025-01-15 20:34:37 +0000

    recreate materialized view

--- supabase/migrations/20241204170257_TES_send_data_to_community_archive.sql
+++ supabase/migrations/20241204170257_TES_send_data_to_community_archive.sql
@@ -326,6 +326,39 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql SECURITY DEFINER
 
+DROP MATERIALIZED VIEW IF EXISTS public.global_activity_summary;
+CREATE MATERIALIZED VIEW
+  public.global_activity_summary AS
+SELECT
+  (
+    SELECT COUNT(DISTINCT account_id) FROM public.account
+  ) AS total_accounts,
+  (
+    SELECT COUNT(DISTINCT tweet_id) FROM public.tweets
+  ) AS total_tweets,
+  (
+    SELECT COUNT(DISTINCT liked_tweet_id) FROM public.likes
+  ) AS total_likes,
+  (
+    SELECT COUNT(DISTINCT tweet_id) FROM public.user_mentions
+  ) AS total_user_mentions,
+  (
+    SELECT json_agg(row_to_json(t))
+    FROM (
+      SELECT * FROM public.get_top_mentioned_users(30)
+    ) t
+  ) AS top_mentioned_users,
+  (
+    SELECT json_agg(row_to_json(t))
+    FROM (
+      SELECT * FROM public.get_top_accounts_with_followers(10)
+    ) t
+  ) AS top_accounts_with_followers,
+  CURRENT_TIMESTAMP AS last_updated;
+
+-- Add a unique index on the last_updated column
+CREATE UNIQUE INDEX idx_global_activity_summary_last_updated 
+ON public.global_activity_summary (last_updated);
 
 
  /* ########### UPDATE OLD FUNCTIONS TO USE ALL_ACCOUNT ########### */
@@ -512,6 +545,19 @@ $$ LANGUAGE plpgsql SECURITY DEFINER
      BEFORE UPDATE ON public.tweet_urls
      FOR EACH ROW 
      EXECUTE FUNCTION update_updated_at_column();
+
+
+ALTER TABLE public.user_mentions
+ADD COLUMN IF NOT EXISTS updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP;
+DROP TRIGGER IF EXISTS update_user_mentions_updated_at ON public.user_mentions;
+CREATE TRIGGER update_user_mentions_updated_at 
+    BEFORE UPDATE ON public.user_mentions
+    FOR EACH ROW 
+    EXECUTE FUNCTION update_updated_at_column();
+
+
+
+
  -- table to disable users from sending live data to the community archive
  CREATE TABLE IF NOT EXISTS public.tes_blocked_scraping_users (
      account_id TEXT PRIMARY KEY ,

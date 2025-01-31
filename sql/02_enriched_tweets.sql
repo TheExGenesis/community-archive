6abf7410e37b0b11bd4737c8dd175fea96ef39fc
TheExGenesis <theexgenesis@gmail.com>
2025-01-31 12:53:36 +0000

    db: enriched tweets

---
+++ sql/02_enriched_tweets.sql
@@ -0,0 +1,27 @@
+
+
+CREATE OR REPLACE VIEW public.enriched_tweets AS
+SELECT 
+    t.tweet_id,
+    t.account_id,
+    a.username,
+    a.account_display_name,
+    t.created_at,
+    t.full_text,
+    t.retweet_count,
+    t.favorite_count,
+    t.reply_to_tweet_id,
+    t.reply_to_user_id,
+    t.reply_to_username,
+    qt.quoted_tweet_id,
+    c.conversation_id,
+    (SELECT p.avatar_media_url
+     FROM profile p 
+     WHERE p.account_id = t.account_id
+     ORDER BY p.archive_upload_id DESC 
+     LIMIT 1) as avatar_media_url,
+    t.archive_upload_id
+FROM tweets t
+JOIN all_account a ON t.account_id = a.account_id
+LEFT JOIN conversations c ON t.tweet_id = c.tweet_id
+LEFT JOIN quote_tweets qt ON t.tweet_id = qt.tweet_id; 

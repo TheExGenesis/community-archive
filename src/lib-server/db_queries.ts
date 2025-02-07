0f9ba3d7480ff990417469f5173e63b50fe06b02
TheExGenesis <theexgenesis@gmail.com>
2025-02-07 15:15:02 +0100

    fix: frontend uploads only to storage

--- src/lib-server/db_queries.ts
+++
@@ -1,7 +0,0 @@
-export const getTweetsCount = async (supabase: any, account_id: string) => {
-  return await supabase
-    .schema('public')
-    .from('account')
-    .select('num_tweets', { count: 'planned' })
-    .eq('account_id', account_id)
-}

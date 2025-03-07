c831d793bff816c44631997d3e7595d3cd4da30b
ri72miieop <106687171+ri72miieop@users.noreply.github.com>
2025-03-07 15:37:41 +0000

    added content to migration file

---
+++ supabase/migrations/20250307143924_optimizations_move_data_edge_function.sql
@@ -0,0 +1,10 @@
+
+
+CREATE INDEX IF NOT EXISTSidx_temp_data_api_types ON temporary_data (inserted, stored) 
+WHERE inserted IS NOT NULL AND stored = 'false' AND type LIKE 'api_%';
+
+ALTER TABLE temporary_data
+ADD COLUMN id INT GENERATED ALWAYS AS IDENTITY;
+
+ALTER TABLE temporary_data
+ADD CONSTRAINT AK_temporary_data_id UNIQUE (id);

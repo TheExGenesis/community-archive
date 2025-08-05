5407f302ad5ab19cba8eaf93538fec4e78aae33b
TheExGenesis <francisco.carvalho@ext.ec.europa.eu>
2025-08-05 17:55:02 +0100

    WIP: Add dev log, benchmarking, services setup, and update configs

---
+++ docs/examples/00_canonical_notebook.py
@@ -0,0 +1,2 @@
+# %%
+# this notebook should have all examples of using community archive data, like a recipe book. Getting single tweets, getting multiple, getting tweets by user, by date range, full text search, getting an entire thread, cleaning for embedding, embedding, clustering, making graphs out of interactions

3e93caa5f657712a91320f50bb8329eebfd25436
ri72miieop <106687171+ri72miieop@users.noreply.github.com>
2025-09-12 21:08:46 +0100

    QT as table instead of materialized view RT table created too

---
+++ sql/tables/16_quote_tweets.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS public.quote_tweets (
+    tweet_id TEXT NOT NULL,
+    quoted_tweet_id TEXT NOT NULL,
+
+     Composite primary key
+    PRIMARY KEY (tweet_id, quoted_tweet_id),
+
+     Foreign key constraints
+    CONSTRAINT fk_quote_tweets_tweet_id FOREIGN KEY (tweet_id) REFERENCES public.tweets (tweet_id) ON DELETE CASCADE
+);
+
+
+CREATE INDEX IF NOT EXISTS idx_quote_tweets_quoted_tweet_id ON public.quote_tweets (quoted_tweet_id);
+CREATE INDEX IF NOT EXISTS idx_quote_tweets_tweet_id ON public.quote_tweets (tweet_id);
